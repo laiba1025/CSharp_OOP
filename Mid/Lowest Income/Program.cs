78eb2c0cdf291e541337a2370bef2ccfc40dfb24
Laiba <123197772+laiba1025@users.noreply.github.com>
2024-04-19 05:13:57 +0200

    Add files via upload

---
+++ Mid/Lowest Income/Program.cs
@@ -0,0 +1,40 @@
+﻿using Program;
+
+namespace TelefonKFT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter FileName");
+                string FileName = Console.ReadLine()!;
+                infile t = new infile(FileName);
+                string isTelenorKft = "No";
+
+                t.First();
+
+                while (!t.End())
+                {
+                    if (t.Current().Payment == 0)
+                    {
+                        isTelenorKft = "Yes";
+                        break;
+                    }
+                    t.Next();   
+                }
+                Console.WriteLine(isTelenorKft);
+
+            } 
+            catch ( FileNotFoundException)
+            { 
+                Console.WriteLine(" file not found");
+
+            }
+
+
+
+        }
+    }
+}
