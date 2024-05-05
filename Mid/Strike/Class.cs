94ccd671ec15d46b2e270de60540ef86bf615b22
Laiba <123197772+laiba1025@users.noreply.github.com>
2024-05-06 01:05:42 +0200

    Add files via upload

---
+++ Mid/Strike/Class.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using TextFile;
+
+
+namespace program {
+	public class Data
+	{
+		public int round;
+		public string name;
+		public int score;
+
+		public Data(int round, string name	, int score)
+		{
+			this.round = round;
+			this.name = name;
+			this.score = score;
+		}
+	}
+	public class tdata
+	{
+		public int Round;
+		public int score;
+		public int count;
+	}
+
+	public class infile
+	{
+		enum Status { success, unSuccess}
+
+		TextFileReader X;
+		Data e;
+		tdata curr = new tdata();
+		bool end;
+		Status st;
+
+
+		private void Read(ref TextFileReader X, ref Status st, ref Data e)
+		{
+			e = new Data(0, "", 0);
+			X.ReadInt(out e.round);
+			X.ReadString(out e.name);
+			if (X.ReadInt(out e.score))
+			{
+				st = Status.success;
+			}
+			else 
+			{
+				st = Status.unSuccess;	
+			}
+
+		}
+
+		public  infile(string FileName)
+		{
+			X = new TextFileReader(FileName);	
+		}
+
+		public void First()
+		{
+			Read(ref X, ref st, ref e);
+			Next();
+		}
+
+		public tdata Current()
+		{
+			return curr;
+		}
+		public bool End()
+		{
+			return end;	
+		}
+
+		public void Next()
+		{
+			end = st == Status.unSuccess;
+			curr.count = 0;
+			curr.Round = e.round;
+			curr.score = 0;
+
+			if (!end) 
+			{
+				while (st == Status.success && (curr.Round  == e.round) )
+				{
+					curr.count ++;
+					if (e.score == 10)
+					{
+						curr.score++;
+					}
+					Read(ref X, ref st, ref e);	
+
+				}
+            }
+			
+		}
+	}
+}
---
+++ Mid/Strike/Program.cs
@@ -0,0 +1,27 @@
+﻿using program;
+
+namespace Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of File");
+            string FileName = Console.ReadLine();   
+            infile t = new infile(FileName);
+            String allStrike = "No";
+            t.First();
+
+            while (!t.End())
+            {
+                if (t.Current().count == t.Current().score) 
+                {
+                   allStrike = "Yes";   
+                    break;
+                }
+                t.Next();   
+            }
+            Console.WriteLine(allStrike);
+        }
+    }
+}
