f05ecf11511dcc764b9ac3b7782d6cea99bead6c
Laiba <123197772+laiba1025@users.noreply.github.com>
2024-05-05 04:18:17 +0200

    Add files via upload

---
+++ Mid/Traffic Analysis/Class.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using TextFile;
+using static Program.Data;
+
+namespace Program
+{
+	public class Data
+	{
+		public int month;
+		public int day;
+		public int vehicles;
+		public Data(int month, int day, int vehicles)
+		{
+			this.month = month;
+			this.day = day;
+			this.vehicles = vehicles;
+		}
+		public class Tdata
+		{
+			public int month;
+			public int Tvehicles;
+		}
+	}
+
+    public class Infile
+    {
+        enum Status
+        {
+            success, unSuccess
+        };
+
+        private TextFileReader X;
+        private Data e;
+        private Tdata curr = new Tdata();
+        bool end;
+        Status st;
+
+        public Infile(string FileName)
+        {
+            X = new TextFileReader(FileName);
+        }
+
+        private void Read(ref TextFileReader X, ref Status st, ref Data e)
+        {
+            e = new Data(0, 0, 0);
+            X.ReadInt(out e.month);
+            X.ReadInt(out e.day);
+            if (X.ReadInt(out e.vehicles))
+            {
+                st = Status.success;
+            }
+            else
+            {
+                st = Status.unSuccess;
+            }
+
+        }
+
+        public void First()
+        {
+            Read(ref X, ref st, ref e);
+            Next();
+        }
+        public bool End()
+        {
+            return end;
+        }
+        public Tdata Current()
+        {
+            return curr;
+        }
+
+        public void Next()
+        {
+            end = st == Status.unSuccess;
+            curr.month = e.month;
+            curr.Tvehicles = 0;
+
+            if (!end)
+            {
+                while (st == Status.success && curr.month == e.month)
+                {
+                    curr.Tvehicles += e.vehicles;
+                    Read(ref X, ref st, ref e);
+                }
+                Console.WriteLine(curr.Tvehicles);
+            }
+        }
+    }
+}
---
+++ Mid/Traffic Analysis/Program.cs
@@ -0,0 +1,33 @@
+﻿using Program;
+using System.Globalization;
+
+namespace Vehicles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter FileName");
+            string FileName = Console.ReadLine();
+            Infile t = new Infile(FileName);    
+            int highest = int.MinValue;
+            int month = 0;
+
+            t.First();
+
+            while (!t.End())
+            {
+                if (t.Current().Tvehicles > highest)
+                {
+                    highest = t.Current().Tvehicles; 
+                    month = t.Current().month;
+
+                }
+                t.Next();   
+            }
+            Console.WriteLine(month);
+
+            
+        }
+    }
+}
