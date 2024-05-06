05c5c9f7d5d88f532a7af180a1b75036fc8382b5
Laiba <123197772+laiba1025@users.noreply.github.com>
2024-05-07 01:22:11 +0200

    Add files via upload

---
+++ Mid/Jhonny's income/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the NAME");
+            string FileName = Console.ReadLine();
+            Infile t = new Infile(FileName);
+            string isChina = "yes";
+            t.First();
+            while (!t.End())
+            {
+                Console.WriteLine(t.Current().income);
+                if (t.Current().income < 30000 )
+                {
+                    isChina = "no";
+                    break;
+                }
+
+                t.Next();
+            }
+            Console.WriteLine(isChina);
+
+        }
+    }
+}
---
+++ Mid/Jhonny's income/class.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using TextFile;
+public class Class1
+{
+    public int month;
+	public string name;
+	public int income;
+
+	public Class1(int month, string name, int income)
+	{
+		this.month = month;	
+		this.name = name;	
+		this.income = income;	
+	}
+
+}
+
+public class Tdata
+{
+	public int month;	
+	public string Name;	
+	public int income;	
+}
+
+public class Infile
+{
+	enum Status
+	{
+		success, unsuccess
+	};
+
+	TextFileReader X;
+	Class1 e;
+	Tdata curr = new Tdata();
+	bool end;
+	Status st;
+
+
+	private void Read(ref TextFileReader X, ref Status st, ref Class1 e)
+	{
+		e = new Class1(0, "", 0);
+        X.ReadInt(out e.month);
+        X.ReadString(out e.name);
+        if (X.ReadInt(out e.income))
+        {
+            st = Status.success;
+        }
+        else
+        {
+            st = Status.unsuccess;
+        }
+
+    }
+	public Infile(string FileName)
+	{
+		X = new TextFileReader(FileName);	
+	}
+	public void First()
+	{
+		Read(ref X, ref st, ref e);
+		Next();
+	}
+	public Tdata Current()
+	{
+		return curr;
+	}
+
+	public bool End()
+	{ 
+		return end; 
+	}
+	public void Next()
+	{
+		end = st == Status.unsuccess;
+		curr.month = e.month;
+		curr.month = 0;
+		
+		if (!end)
+		{
+			while (st == Status.success && curr.month == e.month)
+			{
+				if (e.name == "Jhon")
+				{
+					curr.income += e.income; 
+				} Read(ref X, ref st, ref e);	
+			}
+		}
+	}
+}
