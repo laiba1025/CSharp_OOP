91c66b0f51c9f6e390b0f257e892fe0e9729b4ff
laiba1025 <laibashabbir.2003@gmail.com>
2024-09-02 04:48:53 +0200

    Added Class.cs, Program.cs, and input.txt files

---
+++ Mid/StudentResults/class.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using TextFile;
+
+namespace program
+{
+	public class Data
+	{
+		public string stdId;
+		public int grade;
+
+        public Data(string stdtId, int grade)
+        {
+            this.stdId = stdtId;
+			this.grade = grade;
+        }
+    }
+
+	public class tData
+	{
+		public string St_Id;
+	    public int attempts;
+	}
+
+
+	public class Data2
+	{
+		enum Status
+		{
+			Success, unSuccess
+		};
+		TextFileReader X;
+		Status st;
+		tData curr = new tData();
+		bool end;
+		Data e;
+
+		private void Read(ref TextFileReader X, ref Status st, ref Data e)
+		{
+			e = new Data("", 0);
+			X.ReadString(out e.stdId);
+			if (X.ReadInt(out e.grade))
+			{
+				st = Status.Success;
+			}
+			else
+			{
+				st = Status.unSuccess;
+			}
+
+		}
+
+		public Data2(string fileName)
+		{
+			X = new TextFileReader(fileName);	
+		}
+
+		public tData Current()
+		{
+			return curr;
+		}
+		public bool End()
+		{
+			return end;
+		}
+
+		public void First()
+		{
+			Read (ref X, ref st, ref e);
+			Next();
+		} 
+		public void Next()
+		{
+			end = st == Status.unSuccess;
+			curr.St_Id = e.stdId;
+			curr.attempts= 0;
+			if (!end)
+			{
+				while (st == Status.Success && curr.St_Id == e.stdId)
+				{
+					curr.attempts ++;
+					Read(ref X, ref st, ref e);
+					
+				}
+			}
+		}
+	}
+
+}
---
+++ Mid/StudentResults/program.cs
@@ -0,0 +1,26 @@
+﻿using program;
+
+namespace StudentAttempt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the File Name");
+            string fileName = Console.ReadLine();
+            Data2 t = new Data2(fileName);
+            int Num_Attempts = 0;
+            t.First();
+
+            while (!t.End())
+            {
+                if (Num_Attempts < t.Current().attempts)
+                {
+                    Num_Attempts = t.Current().attempts;
+                }
+                t.Next();
+            }
+            Console.WriteLine(Num_Attempts);
+        }
+    }
+}
