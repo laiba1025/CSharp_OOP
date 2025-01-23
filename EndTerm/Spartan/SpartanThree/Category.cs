f84ca5a166fecd28564d5571e54fdf2c652aac20
laiba1025 <laibashabbir.2003@gmail.com>
2025-01-23 04:27:18 +0100

    First Commit

---
+++ EndTerm/Spartan/SpartanThree/Category.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spartan
+{
+    public interface Category
+    {
+        public virtual String Type() { return ""; }
+    }
+
+    public class Sprint : Category
+    {
+        private static Sprint instance;
+        private static object obj1 = new object();
+
+        public static Sprint Instance()
+        {
+            if (instance == null)
+            {
+                lock (obj1) if (instance == null) instance = new Sprint();
+            }
+            return instance;
+        }
+
+        public String Type() { return "sprint"; }
+
+    }
+
+    public class Beast : Category
+    {
+        private static Beast instance;
+        private static object obj1 = new object();
+
+        public static Beast Instance() 
+        {
+            if (instance == null)
+            {
+                lock(obj1) if (instance == null)  instance = new Beast();
+            }
+            return instance ;
+        }
+        public String Type() { return "beast"; }
+    }
+
+    public class Super : Category
+    {
+        private static Super instance;
+        private static object obj1 = new object ();
+        public static Super Instance()
+        {
+            if (instance == null)
+            {
+                lock(obj1) if (instance == null ) instance = new Super();
+            }
+            return instance;
+        }
+
+        public String Type() { return "super"; }
+        
+    }
+
+
+
+
+
+
+}
---
+++ EndTerm/Spartan/SpartanThree/Competition.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spartan
+{
+    public class Competition
+    {
+        private List<Category> cat = new List<Category>();
+        private List<Result> res = new List<Result>();
+
+        private int year;
+        private String place;
+
+        public Competition(int y, string p, List<Category> c)
+        {
+            if (c.Count() == 0 || y <= 2000) throw new Exception();
+            year = y;
+            place = p;
+            cat = c;
+        }
+
+        public Category PopularCat()
+        {
+            int ind = 0;
+            int MAX = 0;
+            
+            for (int e = 0; e < cat.Count; e++)
+            {
+                int SUM = 0;
+                foreach ( Result e2 in res)
+                {
+                  
+                    if (e2.Cat().Type() == cat[e].Type())
+                    {
+                        SUM++;
+                    }
+
+                    if (MAX < SUM)
+                    {
+                        MAX = SUM;
+                        ind = e;
+                    }
+                }
+            }
+            return cat[ind];
+        }
+
+
+        public void Score(int min, int sec, int num, Category c)
+        {
+            bool l2 = false;
+            bool l3 = false;
+            foreach (Category e in cat)
+            {
+                if (e == c)
+                {
+                    l2 = true;
+                }
+            }
+            if (!l2) { throw new Exception(); }
+            foreach (Result e in res)
+            {
+                if (e.Cat() == c && e.ID() == num)
+                {
+                    l3 = true;
+                }
+            }
+            if (l3) throw new Exception();
+
+            res.Add(new Result(min, sec, num, c));
+        }
+
+        public (bool, int) Winner(Category c)
+        {
+            bool l = false;
+            int ind = 0;
+            int min = 0;
+            for (int e = 0; e < res.Count(); e++)
+            {
+                int temp = res[e].Min()*60 + res[e].Sec();
+                if (l && res[e].Cat() == c)
+                {
+                    if (min > temp)
+                    {
+                        ind = e;
+                    }
+                }
+                if (!l && res[e].Cat() == c)
+                {
+                    l = true;
+                    min = temp;
+                    ind = e;
+                }
+            }
+            if (!l && res.Count == 0) return (false, 0);
+            else return (l, res[ind].ID());
+        }
+    }
+}
