b7bf7022a97bf4897977072f9b8e9de5e4e0c650
laiba1025 <laibashabbir.2003@gmail.com>
2025-01-23 04:21:33 +0100

    First Commit

---
+++ EndTerm/Spartan/SpartanThree/Program.cs
@@ -0,0 +1,121 @@
+﻿using SpartanPractice;
+
+namespace SpartanPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("competition constructor check: ");
+                List<Category> cath = new();
+                Competition comp = new(2009, "salgo", cath);
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - zero categories");
+            }
+            try
+            {
+                Console.Write("competition constructor check: ");
+                List<Category> cath = new();
+                cath.Add(Sprint.Instance());
+                Competition competition = new(1999, "salgo", cath);
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - too small year");
+            }
+            try
+            {
+                Console.Write("competition score check: ");
+                List<Category> cath = new();
+                cath.Add(Sprint.Instance());
+                Competition comp = new(2009, "salgo", cath);
+                comp.Score(1, 1, 1, Super.Instance());
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - nonexisting category");
+            }
+            try
+            {
+                Console.Write("competition score check: ");
+                List<Category> cath = new();
+                cath.Add(Sprint.Instance());
+                Competition comp = new(2009, "salgo", cath);
+                comp.Score(1, 1, 1, Sprint.Instance());
+                comp.Score(1, 2, 1, Sprint.Instance());
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - existing score");
+            }
+            try
+            {
+                Console.Write("result constructor check: ");
+                Result res = new(-2, 2, 1, null);
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - negative min");
+            }
+            try
+            {
+                Console.Write("result constructor check: ");
+                Result res = new(2, -2, 1, null);
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - negative sec");
+            }
+            try
+            {
+                Console.Write("result constructor check: ");
+                Result res = new(2, 60, 1, null);
+                Console.WriteLine("WRONG");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("OK - too big sec");
+            }
+            List<Category> cath2 = new();
+            cath2.Add(Sprint.Instance());
+            cath2.Add(Super.Instance());
+            Competition comp2 = new(2001, "Salgotarjan", cath2);
+            Console.WriteLine("Popular empty: " + (comp2.PopularCat().Type() == "sprint" ? "OK" : "WRONG"));
+
+            bool l;
+            int winner = 0;
+
+            (l, winner) = comp2.Winner(Sprint.Instance());
+            Console.WriteLine("Winner empty: " + (l ? "WRONG" : "OK"));
+
+
+            comp2.Score(10, 8, 4, Super.Instance());
+            comp2.Score(10, 8, 4, Sprint.Instance());
+            (l, winner) = comp2.Winner(Sprint.Instance());
+            Console.WriteLine("Winner 4: " + ((l && winner == 4) ? "OK" : "WRONG"));
+
+            comp2.Score(10, 10, 1, Sprint.Instance());
+            comp2.Score(20, 10, 1, Super.Instance());
+            Console.WriteLine("Popular sprint: " + (comp2.PopularCat().Type() == "sprint" ? "OK" : "WRONG"));
+            (l, winner) = comp2.Winner(Sprint.Instance());
+            Console.WriteLine("Winner 1: " + ((l && winner == 4) ? "OK" : "WRONG"));
+
+            comp2.Score(10, 7, 2, Super.Instance());
+            Console.WriteLine("Popular super: " + (comp2.PopularCat().Type() == "super" ? "OK" : "WRONG"));
+            (l, winner) = comp2.Winner(Super.Instance());
+            Console.WriteLine("Winner 2: " + ((l && winner == 2) ? "OK" : "WRONG"));
+        }
+    }
+}
+
+
---
+++ EndTerm/Spartan/SpartanThree/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartanPractice
+{
+    public class Result
+    {
+        private Category cat;
+        private int min;
+        private int sec;
+        private int id;
+
+        public int GetMin() { return min; }
+        public int GetSec() { return sec; }
+        public int GetId() { return id; }
+
+        public Result(int m , int s, int ID, Category c)
+        {
+            if (m <= 0 || s < 0 || s > 59 || ID <= 0) throw new ArgumentException();
+            this.min = m;
+            this.sec = s;   
+            this.id = ID;   
+            this.cat = c;
+        }
+
+        public Category Cat() { return cat; }
+
+        public int ID () { return id; } 
+
+        public int Min() { return min; }    
+        public int Sec() { return sec; }
+
+    }
+}
